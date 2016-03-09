143ae22f6c376777b0900627f9dcbf966501687f
J. Brandt Buckley <brandt@runlevel1.com>
2016-03-09 08:59:50 -0700

    Don't bother printing shared dir message

--- racadm.sh
+++ racadm.sh
@@ -7,6 +7,6 @@ SHARED="${HOME}/.local/share/racadm-docker"
 # Per the XDG Base Directory Specification, we should use 0700
 mkdir -m 0700 -p "$SHARED"
 
-echo "/shared in the container maps to $SHARED" >&2
+# echo "/shared in the container maps to $SHARED" >&2
 
 docker run --rm -it -v "$SHARED":/shared $DOCKER_NAME /opt/dell/srvadmin/bin/idracadm7 $@
